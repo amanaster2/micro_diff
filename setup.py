74f8bb0c37f3258aab08b22268f7b11e15291045
Amanda Alvis <amanaste@uw.edu>
2023-08-01 16:54:53 -0700

    Uploading folders

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+import setuptools
+
+setup(
+    name="micro",
+    version="0.1",
+    description="Code used to quantify micro-topographical changes on gravel forest roads",
+    packages=setuptools.find_packages(),
+)
